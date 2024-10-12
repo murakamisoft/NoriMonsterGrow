018f35c9ff57f939f2b3a4690ad2c99d2a3b6182
nori <nori@vbminigame.sakura.ne.jp>
2024-10-12 12:17:46 +0900

    first commit

---
+++ backend/src/main/java/com/monstergrow/NoriMonsterGrowApplication.java
@@ -0,0 +1,13 @@
+package com.monstergrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriMonsterGrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoriMonsterGrowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/monstergrow/NoriMonsterGrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monstergrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoriMonsterGrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
